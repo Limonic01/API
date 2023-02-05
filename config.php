7e08fbc82453b177e89601682fe77d0ee877952b
Limonic01 <122029521+Limonic01@users.noreply.github.com>
2023-02-05 19:54:38 +0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$DB = array(
+    "login" => "u0941340_pko317",
+    "password" => "u0941340_pko317",
+    "host" => "localhost",
+    "name" => "u0941340_pko317",
+    "charset" => "utf8mb4"
+);
+
+define("TIME_ZONE", "Asia/Tashkent");
+?>
